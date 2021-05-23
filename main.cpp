b1311a661be5db76d6ec519b4ee6fdadfa662954
Codeissimple <>
2021-05-23 03:10:34 +0000

    final form cleaned up

--- main.cpp
+++ main.cpp
@@ -17,21 +17,14 @@ const long DEC = 12;
 
 const string month_names[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-
 struct date {
    long   year ;
    long   month ;
    long   day ;
 } ;
 
-long compute_julian_day(long, long, long);
-bool is_day_valid(long, long);
-bool is_month_valid(long);
-bool is_day_valid(long);
 bool is_a_leap_year(long);
 long query_for_integer(string);
-long query_for_valid_day(long month) ;
-long query_for_valid_month();
 long return_days_in_a_month(long, long);
 long return_days_in_year(long);
 long query_for_integer(string);
@@ -55,7 +48,6 @@ long return_month(long julian_value){
   long month = 0;
   long year = 0;
 
-
   for (year = 1900; julian_value > return_days_in_year(year); year++){
     julian_value -= return_days_in_year(year);
   }
@@ -66,7 +58,6 @@ long return_month(long julian_value){
 
   return month;
 }
-
 
 long return_day(long julian_value){
   long month = 0;
@@ -81,17 +72,15 @@ long return_day(long julian_value){
   return julian_value;
 }
 
-
 date *return_date(long julian_value){
   date *final_date;
   final_date = new date;
-    final_date->year = return_year(julian_value);
+    final_date->year  = return_year(julian_value);
     final_date->month = return_month(julian_value);
-    final_date->day = return_day(julian_value);
+    final_date->day   = return_day(julian_value);
   return final_date;
 }
 
-// return a pointer to a date
 long return_days_in_year(long year){
   return ((is_a_leap_year(year)) ? 366 : 365);
 }
@@ -100,17 +89,12 @@ bool is_a_leap_year(long year){
 bool return_value = false;
   if ( (year % 400 ==0) || ((year % 4 == 0) && (year % 100 != 0))){
       return_value = true;
-      }
+    }
   else{
       return_value = false;
     }
 return return_value;
 }
-
-
-// OLD PROGRAm code
-
-
 
 long query_for_integer(string prompt) {
 long answer = 0;
@@ -120,18 +104,6 @@ long answer = 0;
 
   return answer;
 }
-/*
-bool is_month_valid(long month){
-bool return_value = false;
-  if (month >= 1 && month <= 12){
-    return_value = true;
-  }
-  else{
-    return_value = false;
-  }
-  return return_value;
-}
-*/
 
 long return_days_in_a_month(long year_provided, long month_provided){
   long return_value =0;
@@ -142,58 +114,15 @@ long return_days_in_a_month(long year_provided, long month_provided){
 
 return return_value;
 }
-/*
-long compute_julian_day(long year_provided, long month_provided, long day_provided){
-
-long julian_number_computed = 0;
-long julian_number = 0;
-
-  for(long year = 1900; year < year_provided; year ++){
-    julian_number_computed += return_days_in_year(year);
-  }
-
-  for(long month = JAN; month < month_provided; month ++){
-    julian_number_computed += return_days_in_a_month(year_provided, month);
-  }
-
-  julian_number_computed += day_provided;
-  
-  return julian_number_computed;
-}
-
-long query_for_valid_month(){
-  long month = 0;
-  do 
-    month = query_for_integer("Enter a month: ");
-  while (is_month_valid(month) == false);
-return month;
-}
-
-bool is_day_valid(long year, long month, long day){
-  if( day < 1 || day > return_days_in_a_month( year, month))
-    return false;
-  else
-    return true;
-}
-
-
-long query_for_valid_day(long year, long month){
-  long day = 0;
-  do
-     day = query_for_integer("Enter a day: ");
-  while (is_day_valid(year, month, day) == false);
-return day;
-}
-*/
 
 int main() {
   long julian_value = 0;
   julian_value = query_for_integer("Enter a Julian value: ");
  
- date *newdate;
- newdate = return_date(julian_value);
+  date *newdate;
+  newdate = return_date(julian_value);
 
- cout << return_month_string(julian_value) << "/" << newdate->day << "/" << newdate->year;
+  cout << newdate->month << "/" << newdate->day << "/" << newdate->year;
   
 return 0;
 }
